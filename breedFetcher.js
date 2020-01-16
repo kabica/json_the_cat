693099b956ac31b580ca380033eab02a14240046
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-16 22:28:56 +0000

    mocha testing now functional

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,49 @@
-// Ultimate one-liner to move files to .gitignore
+// breedFetcher.js
 // touch .gitignore && echo "node_modules/" >> .gitignore && git rm -r --cached node_modules ; git status
 const request = require('request');
-const fs = require('fs');
-const input = process.argv[2];
-const catKey = input.substring(0,4);
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  	//console.log(typeof error);
+  	//console.log(response);
+  	if (error) {
+  		return callback(error, null);
+  	}
+
+    const catObj = JSON.parse(body);
+    const breed = catObj[0];
+
+    //console.log(`TEST: ${catObj[0]['breeds'][0]['description']}`);
+    // callback(error, catObj[0]['breeds'][0]['description']);
+    if (breed) {
+    callback(null, breed.description)    	
+	} else
+	callback('Breed Not Found')
+  });
 
 
 
-request(`https://api.thecatapi.com/v1/images/search?breed_ids=${catKey}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  let catObj = JSON.parse(body);
-  (catObj[0] === undefined) ? console.log('Kant find that Kitty!') : console.log('Description:\n',catObj[0]['breeds'][0]['description']);
-    	});
+};
+
+module.exports = { fetchBreedDescription };
+
+
+
+
+
+
+
+
+
+
+// request(`https://api.thecatapi.com/v1/images/search?breed_ids=${catKey}`, (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   let catObj = JSON.parse(body);
+//   (catObj[0] === undefined) ? console.log('Kant find that Kitty!') : console.log('Description:\n',catObj[0]['breeds'][0]['description']);
+//     	});
 
             
 
